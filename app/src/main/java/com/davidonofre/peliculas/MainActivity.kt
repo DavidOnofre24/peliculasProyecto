d0e65310eae543d83a9ca80db5c6d28c7d31019e
“DavidOnofre24” <davidonofre1010@gmail.com>
2021-05-11 08:26:11 -0500

    Initial commit

---
+++ app/src/main/java/com/davidonofre/peliculas/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.davidonofre.peliculas
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_view)
+    }
+}
